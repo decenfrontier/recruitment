ffe18e6bc4fc0797345e1883714a9b0ab7f9e9fc
ws <ws156858@163.com>
2021-07-08 17:45:01 +0800

    添加job时排除字段

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from jobs.models import Job
 
 class JobAdmin(admin.ModelAdmin):
+    exclude = ('creator', 'created_date', 'modified_date')
     list_display = ('type', 'name', 'city', 'creator', 'created_date', 'modified_date')
+
+    def save_model(self, request, obj, form, change):
+        obj.creator = request.user
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Job, JobAdmin)
