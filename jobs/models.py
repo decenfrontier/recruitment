ebc5814663cb8c01db9d37e6b2643c5982ae9581
ws <ws156858@163.com>
2021-07-08 17:15:16 +0800

    添加修改日期字段 添加默认值

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -18,5 +20,6 @@ class Job(models.Model):
     city = models.SmallIntegerField(choices=Cities, blank=False, verbose_name='工作地点')
     responsibility = models.TextField(max_length=1024, verbose_name='职位职责')
     requirement = models.TextField(max_length=1024, blank=False, verbose_name='职位要求')
-    creator = models.ForeignKey(User, verbose_name='创建人', on_delete=models.SET_NULL)
-    created_date = models.DateTimeField(verbose_name='创建日期')
+    creator = models.ForeignKey(User, verbose_name='创建人', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(verbose_name='创建日期', default=datetime.now)
+    modified_date = models.DateTimeField(verbose_name='修改时间', default=datetime.now)
