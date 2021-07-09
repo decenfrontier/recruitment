c80095f80df5e0d22c8f000c7f311b0b68da2a9a
ws <ws156858@163.com>
2021-07-09 12:25:17 +0800

    优化细节

--- jobs/views.py
+++ jobs/views.py
@@ -6,14 +6,13 @@ from jobs.models import Job
 
 def joblist(request):
     job_list = Job.objects.order_by('type')
-    template = loader.get_template('joblist.html')
     context = {'job_list': job_list}
 
     for job in job_list:
         job.city = Job.Cities[job.city][1]
         job.type = Job.Types[job.type][1]
 
-    return HttpResponse(template.render(context))
+    return render(request, 'joblist.html', context)
 
 
 def detail(request, job_id):
